b00c7635514bc133528f1f47db2d579d9f7a6a80
Jongin Kim <k.jongin@gmail.com>
2024-07-03 15:16:52 +0900

    [refactor] p37

--- UnitTest/baseball_test.cpp
+++ UnitTest/baseball_test.cpp
@@ -1,10 +1,25 @@
 #include <string>
-using namespace std;
 
 #include "pch.h"
 #include "../Baseball/baseball.cpp"
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+using namespace std;
+
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			//PASS
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
